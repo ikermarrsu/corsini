3f57cf878d8ee5f5404060b3341cae8bf6cb6a7a
ikermarrsu <ikermarrsu@gmail.com>
2026-05-13 12:20:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 const menuBtn = document.getElementById('menuBtn');
 const navOverlay = document.getElementById('navOverlay');
 
+let opened = false;
+
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('active');
-    navOverlay.classList.toggle('open');
+    opened = navOverlay.classList.toggle('open');
     
-    if (navOverlay.classList.contains('open')) {
-        document.body.style.overflow = 'hidden';
-    } else {
+    document.body.style.overflow = opened ? 'hidden' : 'auto';
+});
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && opened) {
+        menuBtn.classList.remove('active');
+        navOverlay.classList.remove('open');
         document.body.style.overflow = 'auto';
+        opened = false;
     }
 });
+
